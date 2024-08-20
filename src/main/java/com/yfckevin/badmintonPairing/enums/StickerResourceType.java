66cf5d41fc39e4f1f46d963e3945ef761825f084
yifan <yifanchen0914@gmail.com>
2024-08-20 22:26:18 +0800

    0820 Yifan debug

--- src/main/java/com/yfckevin/badmintonPairing/enums/StickerResourceType.java
+++ src/main/java/com/yfckevin/badmintonPairing/enums/StickerResourceType.java
@@ -1,14 +1,14 @@
 package com.yfckevin.badmintonPairing.enums;
 
 public enum StickerResourceType {
-    static_type("STATIC", "靜態圖片貼圖"),
-    animation("ANIMATION", "動畫貼圖"),
-    sound("SOUND", "含有聲音的貼圖"),
-    animation_sound("ANIMATION_SOUND", "動畫加聲音的貼圖"),
-    popup("POPUP", "彈出式貼圖"),
-    popup_sound("POPUP_SOUND", "彈出加聲音的貼圖"),
-    message("MESSAGE", "可在聊天訊息中展示的貼圖"),
-    rich("RICH", "富貼圖");
+    STATIC("STATIC", "靜態圖片貼圖"),
+    ANIMATION("ANIMATION", "動畫貼圖"),
+    SOUND("SOUND", "含有聲音的貼圖"),
+    ANIMATION_SOUND("ANIMATION_SOUND", "動畫加聲音的貼圖"),
+    POPUP("POPUP", "彈出式貼圖"),
+    POPUP_SOUND("POPUP_SOUND", "彈出加聲音的貼圖"),
+    MESSAGE("MESSAGE", "可在聊天訊息中展示的貼圖"),
+    RICH("RICH", "富貼圖");
 
     private final String value;
     private final String label;
